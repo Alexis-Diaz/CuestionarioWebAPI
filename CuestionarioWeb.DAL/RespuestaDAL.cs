fcb0f32acc23b8ca4793e231ee545b65c0ca2bd4
Alexis-Diaz <alex.d4556@gmail.com>
2021-09-01 17:21:41 -0600

    Se termino de programar la capa DAL

--- CuestionarioWeb.DAL/RespuestaDAL.cs
+++ CuestionarioWeb.DAL/RespuestaDAL.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//
+using CuestionarioWeb.EN;
 
 namespace CuestionarioWeb.DAL
 {
-    class RespuestaDAL
+    public class RespuestaDAL
     {
+        private Contexto _context { get; set; }
+        public RespuestaDAL (Contexto context)
+        {
+            _context = context;
+        }
+
+        //agregar
+        public int GuardarRespuesta(Respuesta respuesta)
+        {
+            if (respuesta != null)
+            {
+                _context.Respuestas.Add(respuesta);
+                return _context.SaveChanges();
+            }
+            return 0;
+        }
+
+        //editar
+        //no se puede editar una respuesta
+
+        //eliminar
+        public int EliminarRespuesta(int? id)
+        {
+            if (id == null || id > 0)
+            {
+                return 0;
+            }
+            Respuesta respuestaEncontrada = _context.Respuestas.Find(id);
+            if (respuestaEncontrada != null)
+            {
+                _context.Respuestas.Remove(respuestaEncontrada);
+                return _context.SaveChanges();
+            }
+            return 0;
+        }
+
+        //buscar por id
+        public Respuesta BuscarRespuestaPorId(int? id)
+        {
+            if (id == null || id < 0)
+            {
+                return null;
+            }
+            return _context.Respuestas.Find(id);
+        }
+
+        //listar
+        public List<Respuesta> ListarRespuestas()
+        {
+            return _context.Respuestas.ToList();
+        }
     }
 }
